4f21df15500ba63186ea4d3a2ab341f04c19c360
Jitske Van Peer <jitskevanpeer@gmail.com>
2023-11-24 13:41:16 +0100

    unit test xor

--- playground_testing.py
+++ playground_testing.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.io as spio
 
 def lookup_generate(dim, n_keys, mode = 1):
     table = np.empty((0, dim)) 
@@ -40,8 +41,39 @@ def test_matrix_probability(LUT,in_p):
     plt.show()
 
 
-LUT,p_in = lookup_generate(1024,256,1)
-test_matrix_probability(LUT,p_in)
+#LUT,p_in = lookup_generate(1024,256,1)
+#test_matrix_probability(LUT,p_in)
+
+dim = 1024
+#position_table = lookup_generate(dim, len(position_table), 1)
+#grayscale_table = lookup_generate(dim, len(position_table), 0)
+mat = spio.loadmat('XOR_test_data.mat', squeeze_me=True)
+
+in1 = mat['in1'] # array
+in2 = mat['in2']
+desired = mat['result']
+
+def encode_HDC_RFF(img, position_table, grayscale_table, dim):
+    img_hv = np.zeros(dim, dtype=np.int16)
+    container = np.zeros((len(position_table), dim))
+    for pixel in range(len(position_table)):
+        #Get the input-encoding and XOR-ing result:  
+        xor_result = (grayscale_table != position_table).astype(int)
+
+        #hv = # -> INSERT YOUR CODE
+        #container[pixel, :] = hv*1
+        
+    #img_hv = np.sum(container, axis = 0) #bundling without the cyclic step yet
+    return xor_result
 
 
+def test_XOR(in1,in2,desired,dim):
+    calculated = encode_HDC_RFF([0], in1, in2, dim)
+    print("desired =",desired)
+    print("calculated =",calculated)
+    if (desired == calculated).all():
+        print("XOR result is correct")
+    else:
+        print("XOR result is different")
 
+test_XOR(in1,in2,desired,dim)
